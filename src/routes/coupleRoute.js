748d2d0e2c5148dc9586a56765a0521c2e4ce971
DCTP-BE <akpata.thelma@gmail.com>
2024-08-31 22:04:23 +0100

    server update fix

--- src/routes/coupleRoute.js
+++ src/routes/coupleRoute.js
@@ -4,6 +4,8 @@ const coupleController = require('../controllers/coupleController');
 
 router.post('/api/v1/couple-register', coupleController.register);
 router.get('/api/v1/couple-all', coupleController.getAll);
+router.get('/api/v1/couple/:id', coupleController.getCoupleById);
+router.put('/api/v1/couple/:id', coupleController.updateCouple);
 router.post('/api/v1/couple-login', coupleController.login);
 
 module.exports = router;
