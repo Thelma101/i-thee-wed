c217578589abd65a5964f85f942e091cfb30e257
DCTP-BE <akpata.thelma@gmail.com>
2024-08-31 12:31:50 +0100

    postgres dependenciy

--- src/models/coupleModel.js
+++ src/models/coupleModel.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-// const sequelize = require('../config/db');
+const sequelize = require('../config');
 
 const Couple = sequelize.define('Couple', {
   first_name: {
