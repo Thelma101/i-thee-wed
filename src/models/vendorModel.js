c0b0d69ce6af48b57412f7bd54802f49a15fb665
DCTP-BE <akpata.thelma@gmail.com>
2024-09-04 21:09:13 +0100

    vendor management update

--- src/models/vendorModel.js
+++ src/models/vendorModel.js
@@ -1,1 +1,21 @@
-const mongoose = require('mongoose');
+const { dataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const Vendor = sequelize.define('Vendor', {
+    id: {
+        type: dataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name: {
+        type: dataTypes.STRING,
+        allowNull: false,
+    },
+    address: {
+        type: dataTypes.STRING,
+        allowNull: false,
+    },
+    phone: {
+        type: dataTypes.STRING, // allowNull: false,
+})
+}
