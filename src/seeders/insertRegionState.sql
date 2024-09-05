5d8439179aef068d8b1c2c3edfe285796d134613
DCTP-BE <akpata.thelma@gmail.com>
2024-09-05 12:54:40 +0100

    region and state model fix

---
+++ src/seeders/insertRegionState.sql
@@ -0,0 +1,62 @@
+-- seedRegionStateData.sql
+
+-- Insert Regions
+INSERT INTO regions (name) VALUES
+('East'),
+('West'),
+('North'),
+('South'),
+('Central');
+
+-- Insert States
+
+-- East Region
+INSERT INTO states (name, region_id) VALUES
+('Abia', (SELECT id FROM regions WHERE name = 'East')),
+('Anambra', (SELECT id FROM regions WHERE name = 'East')),
+('Ebonyi', (SELECT id FROM regions WHERE name = 'East')),
+('Enugu', (SELECT id FROM regions WHERE name = 'East')),
+('Imo', (SELECT id FROM regions WHERE name = 'East'));
+
+-- West Region
+INSERT INTO states (name, region_id) VALUES
+('Ekiti', (SELECT id FROM regions WHERE name = 'West')),
+('Lagos', (SELECT id FROM regions WHERE name = 'West')),
+('Ogun', (SELECT id FROM regions WHERE name = 'West')),
+('Ondo', (SELECT id FROM regions WHERE name = 'West')),
+('Osun', (SELECT id FROM regions WHERE name = 'West')),
+('Oyo', (SELECT id FROM regions WHERE name = 'West'));
+
+-- North Region
+INSERT INTO states (name, region_id) VALUES
+('Adamawa', (SELECT id FROM regions WHERE name = 'North')),
+('Bauchi', (SELECT id FROM regions WHERE name = 'North')),
+('Borno', (SELECT id FROM regions WHERE name = 'North')),
+('Gombe', (SELECT id FROM regions WHERE name = 'North')),
+('Jigawa', (SELECT id FROM regions WHERE name = 'North')),
+('Kaduna', (SELECT id FROM regions WHERE name = 'North')),
+('Kano', (SELECT id FROM regions WHERE name = 'North')),
+('Katsina', (SELECT id FROM regions WHERE name = 'North')),
+('Kebbi', (SELECT id FROM regions WHERE name = 'North')),
+('Sokoto', (SELECT id FROM regions WHERE name = 'North')),
+('Yobe', (SELECT id FROM regions WHERE name = 'North')),
+('Zamfara', (SELECT id FROM regions WHERE name = 'North'));
+
+-- South Region
+INSERT INTO states (name, region_id) VALUES
+('Akwa Ibom', (SELECT id FROM regions WHERE name = 'South')),
+('Bayelsa', (SELECT id FROM regions WHERE name = 'South')),
+('Cross River', (SELECT id FROM regions WHERE name = 'South')),
+('Delta', (SELECT id FROM regions WHERE name = 'South')),
+('Edo', (SELECT id FROM regions WHERE name = 'South')),
+('Rivers', (SELECT id FROM regions WHERE name = 'South'));
+
+-- Central Region
+INSERT INTO states (name, region_id) VALUES
+('Abuja', (SELECT id FROM regions WHERE name = 'Central')),
+('Benue', (SELECT id FROM regions WHERE name = 'Central')),
+('Kogi', (SELECT id FROM regions WHERE name = 'Central')),
+('Kwara', (SELECT id FROM regions WHERE name = 'Central')),
+('Nasarawa', (SELECT id FROM regions WHERE name = 'Central')),
+('Niger', (SELECT id FROM regions WHERE name = 'Central')),
+('Plateau', (SELECT id FROM regions WHERE name = 'Central'));
