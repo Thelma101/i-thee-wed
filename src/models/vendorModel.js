8cedd936a37664fb19267f7591b14390e2f1280b
DCTP-BE <akpata.thelma@gmail.com>
2024-09-05 09:59:28 +0100

    vendor schema fix

--- src/models/vendorModel.js
+++ src/models/vendorModel.js
@@ -1,4 +1,46 @@
 const { dataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
-const 
+const Vendor = sequelize.define('Vendor' {
+    business_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    category: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    regoin: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    state: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
+        }
+    },
+    phone_number: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isNumeric: true,
+            len: [10, 15]
+        }
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            minLength: 8
+        }
+    },
+    
+}) 
