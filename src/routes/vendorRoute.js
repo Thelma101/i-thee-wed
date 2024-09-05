850863422494371fa554781cfb1ac23b5e3cfa3a
DCTP-BE <akpata.thelma@gmail.com>
2024-09-05 23:11:57 +0100

    region and state model fix

--- src/routes/vendorRoute.js
+++ src/routes/vendorRoute.js
@@ -3,5 +3,7 @@ const express = require('express');
 const router = express.Router();
 const Vendor = require('../controllers/vendorController');
 
-router.post('/api/v1/vendor-register', Vendor.createVendor);
+router.post('/api/v1/vendor-register', Vendor.registerVendor);
 router.get('/api/v1/vendor-all', Vendor.getAllVendors);
+
+module.exports=router;
