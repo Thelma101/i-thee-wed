abbc190f24d0800d6104e5c964ae9737e14189dc
DCTP-BE <akpata.thelma@gmail.com>
2024-09-05 22:04:00 +0100

    region and state model fix

---
+++ src/config/syncDatabase.js
@@ -0,0 +1,15 @@
+
+const sequelize = require('./config/database'); 
+const Couple = require('../models/coupleModel');
+const Vendor = require('./models/vendorModel');
+
+const syncDatabase = async () => {
+    try {
+        await sequelize.sync({ force: false });
+        console.log('Database synchronized');
+    } catch (error) {
+        console.error('Error synchronizing database:', error);
+    }
+};
+
+syncDatabase();
