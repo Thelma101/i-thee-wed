c052553cc1e762850be3f7746553d0fe767503f0
DCTP-BE <akpata.thelma@gmail.com>
2024-09-05 09:52:22 +0100

    vendor schema fix

--- src/models/vendorModel.js
+++ src/models/vendorModel.js
@@ -1,21 +1,4 @@
 const { dataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
-const Vendor = sequelize.define('Vendor', {
-    id: {
-        type: dataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-    },
-    name: {
-        type: dataTypes.STRING,
-        allowNull: false,
-    },
-    address: {
-        type: dataTypes.STRING,
-        allowNull: false,
-    },
-    phone: {
-        type: dataTypes.STRING, // allowNull: false,
-})
-}
+const 
