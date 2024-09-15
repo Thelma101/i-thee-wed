23fda9d1efe37280ca069644ea19cadf737b6e61
DCTP-BE <akpata.thelma@gmail.com>
2024-09-15 09:26:03 +0100

    Fix: couple validation update

--- src/middlewares/validators/couple/authValidation.js
+++ src/middlewares/validators/couple/authValidation.js
@@ -1,21 +1,30 @@
 const Joi = require('joi');
 
+// const registerCoupleSchema = Joi.object({
+//     username: Joi.string().min(4).alphanum().required().messages({
+//         'string.min': 'Username must be at least 4 characters long',
+//         'string.alphanum': 'Username must contain only alphanumeric characters',
+//     }),
+
 const registerCoupleSchema = Joi.object({
-    username: Joi.string().min(4).alphanum().required().messages({
-        'string.min': 'Username must be at least 4 characters long',
-        'string.alphanum': 'Username must contain only alphanumeric characters'
-    }),
+    username: Joi.string()
+        .pattern(/^(?![_\-&.])[a-zA-Z0-9._\-&]{4,30}(?<![_\-&.])$/)
+        .required()
+        .messages({
+            'string.pattern.base': 'Username must be 4-30 characters long and can only contain letters, numbers, dots, underscores, hyphens, and ampersands, but cannot start or end with special characters',
+        }),
+
     phone_number: Joi.string()
         .pattern(/^\+?[0-9]{10,11}$/)
         .required()
         .messages({
             'string.pattern.base': 'Phone number must include country code and be 10 to 11 digits long'
         }),
+
     password: Joi.string().min(6).required().messages({
         'string.min': 'Password must be at least 6 characters long'
     })
 });
-
 
 const updateCoupleSchema = Joi.object({
     first_name: Joi.string().optional(),
@@ -23,9 +32,13 @@ const updateCoupleSchema = Joi.object({
     email: Joi.string().email().optional().messages({
         'string.email': 'Email must be a valid email address'
     }),
-    phone_number: Joi.string().pattern(/^(\+?\d{1,3})?\d{10,11}$/).required().messages({
-        'string.pattern.base': 'Phone number must include country code and be 10 to 11 digits long'
-    }),
+    
+    phone_number: Joi.string()
+        .pattern(/^(\+?\d{1,3})?\d{10,11}$/)
+        .required()
+        .messages({
+            'string.pattern.base': 'Phone number must include country code and be 10 to 11 digits long'
+        }),
 });
 
 module.exports = {
