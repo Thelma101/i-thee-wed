ab83a711645e24a44ec47fa9be2f45ededf0254e
DCTP-BE <akpata.thelma@gmail.com>
2024-09-15 09:07:46 +0100

    Fix: couple validation update

--- src/middlewares/validators/couple/authValidation.js
+++ src/middlewares/validators/couple/authValidation.js
@@ -5,9 +5,12 @@ const registerCoupleSchema = Joi.object({
         'string.min': 'Username must be at least 4 characters long',
         'string.alphanum': 'Username must contain only alphanumeric characters'
     }),
-    phone_number: Joi.string().pattern(/^(\+?\d{1,3})?\d{10,11}$/).required().messages({
-        'string.pattern.base': 'Phone number must include country code and be 10 to 11 digits long'
-    }),
+    phone_number: Joi.string()
+        .pattern(/^\+?[0-9]{10,11}$/)
+        .required()
+        .messages({
+            'string.pattern.base': 'Phone number must include country code and be 10 to 11 digits long'
+        }),
     password: Joi.string().min(6).required().messages({
         'string.min': 'Password must be at least 6 characters long'
     })
