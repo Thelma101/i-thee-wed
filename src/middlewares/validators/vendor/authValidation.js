b7b61725c8660b29ca08a0176c2302d402256980
DCTP-BE <akpata.thelma@gmail.com>
2024-09-16 22:50:41 +0100

    Fix: Vendor service update

--- src/middlewares/validators/vendor/authValidation.js
+++ src/middlewares/validators/vendor/authValidation.js
@@ -7,7 +7,7 @@ const registerVendorSchema = Joi.object({
         .pattern(/^([0]{1}[7-9]{1}[0-9]{9}|[7-9]{1}[0-9]{9})$/)
         .required(),  // Nigerian phone number validation
     password: Joi.string().min(6).required(),  // Ensure password strength
-    category_id: Joi.number().integer().optional(),  // Optional for initial registration
+    category_id: Joi.number().integer().optional(),
     state_id: Joi.number().integer().optional(),
     email: Joi.string().email().optional(),
     address: Joi.string().optional(),
